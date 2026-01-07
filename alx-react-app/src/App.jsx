d54eb2e0508f0b69825b66d6fd6e8b98fb8c1487
Emmanuel <blaemedia26@gmail.com>
2026-01-07 19:37:27 +0100

    Create a User Profile Card Using Props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,6 +5,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from "./components/UserProfile";
 import './App.css'
 
 function App() {
@@ -17,6 +18,11 @@ function App() {
       <MainContent />
       <Footer />
       <WelcomeMessage />
+      <UserProfile
+        name="Alice"
+        age={25}
+        bio="Loves hiking and photography"
+      />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
