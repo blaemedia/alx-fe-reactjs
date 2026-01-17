c64b7d1fdce48bc1086e19f6deae59d064dd79e7
Emmanuel <blaemedia26@gmail.com>
2026-01-17 02:21:06 +0100

    Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from "./components/UserProfile";
 import ProfilePage from "./ProfilePage";
-import UserContext from "./UserContext";
+import UserContext from "../UserContext";
 import './App.css'
 
 function App() {
