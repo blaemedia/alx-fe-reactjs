562dd8d030d57c63b44dd445fa20e0227712d344
Emmanuel <blaemedia26@gmail.com>
2026-01-18 06:18:11 +0100

    Implementing Custom Permissions in Django

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,14 @@
-function UserProfile(props) {
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
+function UserProfile() {
+  const { user } = useContext(UserContext);
+
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>User Profile</h2>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
     </div>
   );
 }
