0ad3877c19221890062783bd333959a50cccf2cf
Emmanuel <blaemedia26@gmail.com>
2026-02-12 16:10:49 +0100

    Building the Recipe Detail Page

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import recipesData from "../data.json";
 
 function HomePage() {
@@ -16,24 +17,21 @@ function HomePage() {
 
       <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
-          <div
-            key={recipe.id}
-            className="bg-white rounded-lg shadow-md hover:shadow-xl transform hover:scale-105 transition duration-300"
-          >
-            <img
-              src={recipe.image}
-              alt={recipe.title}
-              className="w-full h-48 object-cover rounded-t-lg"
-            />
-            <div className="p-4">
-              <h2 className="text-lg font-semibold text-gray-800 mb-2">
-                {recipe.title}
-              </h2>
-              <p className="text-gray-600 text-sm">
-                {recipe.summary}
-              </p>
+          <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
+            <div className="bg-white rounded-lg shadow-md hover:shadow-xl transform hover:scale-105 transition duration-300 cursor-pointer">
+              <img
+                src={recipe.image}
+                alt={recipe.title}
+                className="w-full h-48 object-cover rounded-t-lg"
+              />
+              <div className="p-4">
+                <h2 className="text-lg font-semibold text-gray-800 mb-2">
+                  {recipe.title}
+                </h2>
+                <p className="text-gray-600 text-sm">{recipe.summary}</p>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
